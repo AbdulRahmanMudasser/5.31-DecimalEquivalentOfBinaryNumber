ee8217c288c5e3d959b9537d9198453f5a7c5f3d
Abdul Rahman <abdulrahmanmuddasser@gmail.com>
2023-04-29 22:09:34 +0500

    declared variable + getting input from user

--- Decimal Equivalent of Binary Number/Program.cs
+++ Decimal Equivalent of Binary Number/Program.cs
@@ -2,7 +2,12 @@
 {
     static void Main(string[] args)
     {
+        // declaring variables
+        string binary;
 
+        // getting input from user
+        Console.Write("Enter A Binary Number (Containing Only 0s And 1s):  ");
+        binary = Console.ReadLine();
     }
     // end of main method
 }
