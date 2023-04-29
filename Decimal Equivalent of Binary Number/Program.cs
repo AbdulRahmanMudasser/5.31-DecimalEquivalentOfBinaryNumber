9979dc9986101102bd89cb302f03e0f17f251848
Abdul Rahman <abdulrahmanmuddasser@gmail.com>
2023-04-29 22:16:20 +0500

    solution done

--- Decimal Equivalent of Binary Number/Program.cs
+++ Decimal Equivalent of Binary Number/Program.cs
@@ -3,7 +3,9 @@
     static void Main(string[] args)
     {
         // declaring variables
-        string binary;
+        string? binary;
+        int decimalValue = 0;
+        int exponent = 0;
 
         // getting input from user
         Console.Write("Enter A Binary Number (Containing Only 0s And 1s):  ");
@@ -12,15 +14,27 @@
         Console.WriteLine();    // just for spacing
 
         // check if the entered number is binary or not
-        foreach (char c in binary)
+        foreach (char c in binary!)
         {
-            if (c != 0 && c != 1)
+            if (c != '0' && c != '1')
             {
                 Console.WriteLine("Error - Enter Number Is Not A Binary Number");
 
                 return;
             }
         }
+
+        for (int i = binary.Length - 1; i >= 0; i--) 
+        {
+            if (binary[i] == '1')
+            {
+                decimalValue = decimalValue + (int)Math.Pow(2, exponent);
+            }
+
+            exponent++;
+        }
+
+        Console.WriteLine("Decimal equivalent: " + decimalValue);
     }
     // end of main method
 }
