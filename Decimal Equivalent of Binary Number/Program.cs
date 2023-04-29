52b95db92c96fc723cfd6eb82d2c8e76d1360244
Abdul Rahman <abdulrahmanmuddasser@gmail.com>
2023-04-29 22:11:34 +0500

    check if the entered number is binary or not

--- Decimal Equivalent of Binary Number/Program.cs
+++ Decimal Equivalent of Binary Number/Program.cs
@@ -8,6 +8,19 @@
         // getting input from user
         Console.Write("Enter A Binary Number (Containing Only 0s And 1s):  ");
         binary = Console.ReadLine();
+
+        Console.WriteLine();    // just for spacing
+
+        // check if the entered number is binary or not
+        foreach (char c in binary)
+        {
+            if (c != 0 && c != 1)
+            {
+                Console.WriteLine("Error - Enter Number Is Not A Binary Number");
+
+                return;
+            }
+        }
     }
     // end of main method
 }
